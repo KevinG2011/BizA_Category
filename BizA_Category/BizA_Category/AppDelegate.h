caa541b7c77e7fb413ce3a825a2c426337be0a00
lijia <lijia@huajiao.tv>
2019-02-01 12:39:07 +0800

    init BizA Category

---
+++ BizA_Category/BizA_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BizA_Category
+//
+//  Created by lijia on 2019/2/1.
+//  Copyright © 2019 MJHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BizA_Category/BizA_Category/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BizA_Category
+//
+//  Created by lijia on 2019/2/1.
+//  Copyright © 2019 MJHF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
