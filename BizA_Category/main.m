d34a34b063a746289e2c465551c325eda649fd4d
lijia <lijia@huajiao.tv>
2019-02-01 16:47:26 +0800

    add main.m

---
+++ BizA_Category/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ModulizationTest
+//
+//  Created by lijia on 2019/2/1.
+//  Copyright © 2019 MJHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return 0;
+    }
+}
